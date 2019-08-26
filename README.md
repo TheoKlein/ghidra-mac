6445953501a6fa683504b75eda6bdf6e541e1111
TheoKlein <theokleintw@gmail.com>
2019-08-26 15:44:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ghidra-Mac
+Pack Ghidra with OpenJDK into a standalone MacOS application.
+
+The `Launcher-Template` is reference from https://gist.github.com/yifanlu/e9965cdb148b550335e57899f790cad2#file-ghidra-osx-launcher-template-tgz
+
+## Usage
+```
+$ curl https://github.com/TheoKlein/ghidra-mac/install.sh | sh
+```
---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+LAUNCHER="https://github.com/TheoKlein/ghidra-mac/"
+GHIDRA="https://ghidra-sre.org/ghidra_9.0.4_PUBLIC_20190516.zip"
+OPEN_JDK="https://download.java.net/java/GA/jdk12.0.2/e482c34c86bd4bf8b56c0b35558996b9/10/GPL/openjdk-12.0.2_osx-x64_bin.tar.gz"
+
+cd /tmp
+
+# Launcher Template
+wget $LAUNCHER && \
+tar zxvf Ghidra-OSX-Launcher-Template.tgz
+
+# Official ghidra
+wget $GHIDRA -O ghidra.zip && \
+unzip ghidra.zip -d ./Ghidra.app/ && \
+mv ./Ghidra.app/ghidra* ./Ghidra.app/ghidra
+
+# Official OpenJDK
+wget $OPEN_JDK -O openjdk.tar.gz && \
+tar zxvf openjdk.tar.gz -C Ghidra.app/ && \
+mv ./Ghidra.app/jdk* ./Ghidra.app/jdk
+
+# Move app to /Applications and cleanup
+mv Ghidra.app /Applications
+rm ghidra.zip openjdk.tar.gz Ghidra-OSX-Launcher-Template.tgz
